3cd587f71ed76bf763d700b535e9103ef8c7d9be
pr773 <ps039796@gmail.com>
2026-03-12 05:08:39 +0000

    day 4

---
+++ @/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+ import Navbar from "./Components/Navbar";
+function App() {
+  const [count, setCount] = useState(0)
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Hello React</h1>
+    </div>
+  );
+}
+}
+export default App
+
